46f23ae63aed5adffdd13c5880225c7c18d8bcc1
Roy Palnadi <revannaldi45@gmail.com>
2025-03-08 23:43:28 +0700

    build

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  distDir: "out",
   reactStrictMode: true,
   swcMinify: true,
-  basePath: process.env.PAGES_BASE_PATH,
-  images: {
-    unoptimized: true, // Hindari masalah gambar saat export
-  },
 };
 
 module.exports = nextConfig;
